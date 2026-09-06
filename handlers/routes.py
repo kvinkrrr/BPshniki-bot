cf4effedebd29d5c7fefb00e5b41c0ca4d8c4f2b
kvinkrrr <classmates3a@yandex.ru>
2026-09-06 20:19:43 +0300

    1.2

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,6 +1,5 @@
 import logging
 import random
-from aiogram.filters import CommandStart
 from aiogram.types import (
     ChatJoinRequest)
 from aiogram import Router, F
@@ -17,194 +16,7 @@ GROUP_ID = -1001870935202
 BOT_TOKEN = "8064135284:AAFrlUp07n7oyuwwRmhbltJzxIvAQ-Xc1zk"
 router = Router()
 
-group_to_user = {}
 
-# ID сообщения пользователя -> ID сообщения в группе
-user_to_group = {}
-
-class SendMessage(StatesGroup):
-    waiting_for_message = State()
-
-@router.message(Command("send"))
-async def send_command(message:Message, state: FSMContext):
-    caption = "-У тебя возникла проблема 💎?\nМы её постараемся решить 🚀. Опиши свою проблему, "\
-              "а также свой @username для того, чтобы мы могли с тобой связаться 👇.\n"\
-              "После того, как придет сообщение от админа группы - свайпни влево для ответа🔜.\n\n"\
-              "Нажми /start, чтобы вернутся обратно в меню\n"
-
-    emojis = [
-        ("💎", "5344002312039862645"),
-        ("🚀", "5345973749273356952"),
-        ("👇", "5256258204651785650"),
-        ("🔜", "5442990914391791134")
-    ]
-
-    entities = []
-
-    # Добавляем Premium Emoji
-    for emoji, emoji_id in emojis:
-        pos = caption.find(emoji)
-
-        entities.append(
-            MessageEntity(
-                type="custom_emoji",
-                offset=len(
-                    caption[:pos].encode("utf-16-le")
-                ) // 2,
-                length=len(
-                    emoji.encode("utf-16-le")
-                ) // 2,
-                custom_emoji_id=emoji_id
-            )
-        )
-
-    await message.answer_photo(
-        photo=FSInputFile("sva.jpg"),
-        caption=caption,
-        caption_entities=entities)
-
-
-
-    await state.set_state(
-        SendMessage.waiting_for_message
-    )
-
-# =========================
-# ОТПРАВКА СООБЩЕНИЯ В ГРУППУ
-# =========================
-
-@router.message(SendMessage.waiting_for_message)
-async def process_message(
-    message: types.Message,
-    state: FSMContext,
-    bot: Bot
-):
-
-    try:
-
-        # Копируем сообщение пользователя в группу
-        sent = await bot.copy_message(
-            chat_id=GROUP_AD,
-            from_chat_id=message.chat.id,
-            message_id=message.message_id
-        )
-
-        # Связываем сообщение в группе с пользователем
-        group_to_user[sent.message_id] = message.from_user.id
-
-        # Связываем сообщение пользователя с сообщением группы
-        user_to_group[message.message_id] = sent.message_id
-
-        await message.answer(
-            "✅ Сообщение отправлено!\n"
-            "Если хочешь ответить на сообщение от админа, то свайпни влево для ответа\n\n"
-            "Нажми /start, чтобы вернутся обратно в меню\n"
-        )
-
-    except Exception as e:
-
-        await message.answer(
-            f"❌ Ошибка:\n{e}"
-        )
-
-    finally:
-
-        await state.clear()
-
-# ==================================================
-# ОТВЕТ УЧАСТНИКА ГРУППЫ
-# ==================================================
-
-@router.message(
-    lambda message:
-    message.chat.id == GROUP_AD
-    and message.reply_to_message is not None
-)
-async def group_reply(message: types.Message, bot: Bot):
-
-    replied_message_id = (
-        message.reply_to_message.message_id
-    )
-
-    # Ищем пользователя, которому принадлежит
-    # сообщение, на которое ответили
-    user_id = group_to_user.get(
-        replied_message_id
-    )
-
-    if not user_id:
-        return
-
-    try:
-
-        # Отправляем ответ пользователю
-        sent = await bot.copy_message(
-            chat_id=user_id,
-            from_chat_id=GROUP_AD,
-            message_id=message.message_id
-        )
-
-        # Теперь сообщение пользователя в личке
-        # связано с сообщением в группе
-        user_to_group[sent.message_id] = (
-            message.message_id
-        )
-
-    except Exception as e:
-
-        print(
-            f"Ошибка отправки ответа: {e}\n\n"
-            "Нажми /start, чтобы вернутся обратно в меню\n"
-        )
-
-# ==================================================
-# ОТВЕТ ПОЛЬЗОВАТЕЛЯ НА СООБЩЕНИЕ ИЗ ГРУППЫ
-# ==================================================
-
-@router.message(
-    lambda message:
-    message.chat.id != GROUP_AD
-    and message.reply_to_message is not None
-)
-async def user_reply(message: types.Message, bot: Bot):
-
-    replied_message_id = (
-        message.reply_to_message.message_id
-    )
-
-    # Находим сообщение в группе,
-    # которому соответствует сообщение в личке
-    group_message_id = user_to_group.get(
-        replied_message_id
-    )
-
-    if not group_message_id:
-        return
-
-    try:
-
-        # Копируем ответ пользователя в группу
-        sent = await bot.copy_message(
-            chat_id=GROUP_AD,
-            from_chat_id=message.chat.id,
-            message_id=message.message_id,
-            reply_parameters=types.ReplyParameters(
-                message_id=group_message_id
-            )
-        )
-
-        # Связываем новое сообщение в группе
-        # с пользователем
-        group_to_user[sent.message_id] = (
-            message.from_user.id
-        )
-
-    except Exception as e:
-
-        print(
-            f"Ошибка отправки ответа: {e}\n\n"
-            "Нажми /start, чтобы вернутся обратно в меню\n"
-        )
 
 # ==========================================
 # ПРОВЕРОЧНЫЕ ТЕКСТЫ
@@ -415,4 +227,193 @@ async def verification_answer(message: Message, bot:Bot):
             "Скопируй текст из предыдущего сообщения "
             "и отправь его без изменений.",
             parse_mode="HTML"
+        )
+
+group_to_user = {}
+
+# ID сообщения пользователя -> ID сообщения в группе
+user_to_group = {}
+
+class SendMessage(StatesGroup):
+    waiting_for_message = State()
+
+@router.message(Command("send"))
+async def send_command(message:Message, state: FSMContext):
+    caption = "-У тебя возникла проблема 💎?\nМы её постараемся решить 🚀. Опиши свою проблему, "\
+              "а также свой @username для того, чтобы мы могли с тобой связаться 👇.\n"\
+              "После того, как придет сообщение от админа группы - свайпни влево для ответа🔜.\n\n"\
+              "Нажми /start, чтобы вернутся обратно в меню\n"
+
+    emojis = [
+        ("💎", "5344002312039862645"),
+        ("🚀", "5345973749273356952"),
+        ("👇", "5256258204651785650"),
+        ("🔜", "5442990914391791134")
+    ]
+
+    entities = []
+
+    # Добавляем Premium Emoji
+    for emoji, emoji_id in emojis:
+        pos = caption.find(emoji)
+
+        entities.append(
+            MessageEntity(
+                type="custom_emoji",
+                offset=len(
+                    caption[:pos].encode("utf-16-le")
+                ) // 2,
+                length=len(
+                    emoji.encode("utf-16-le")
+                ) // 2,
+                custom_emoji_id=emoji_id
+            )
+        )
+
+    await message.answer_photo(
+        photo=FSInputFile("sva.jpg"),
+        caption=caption,
+        caption_entities=entities)
+
+
+
+    await state.set_state(
+        SendMessage.waiting_for_message
+    )
+
+# =========================
+# ОТПРАВКА СООБЩЕНИЯ В ГРУППУ
+# =========================
+
+@router.message(SendMessage.waiting_for_message)
+async def process_message(
+    message: types.Message,
+    state: FSMContext,
+    bot: Bot
+):
+
+    try:
+
+        # Копируем сообщение пользователя в группу
+        sent = await bot.copy_message(
+            chat_id=GROUP_AD,
+            from_chat_id=message.chat.id,
+            message_id=message.message_id
+        )
+
+        # Связываем сообщение в группе с пользователем
+        group_to_user[sent.message_id] = message.from_user.id
+
+        # Связываем сообщение пользователя с сообщением группы
+        user_to_group[message.message_id] = sent.message_id
+
+        await message.answer(
+            "✅ Сообщение отправлено!\n"
+            "Если хочешь ответить на сообщение от админа, то свайпни влево для ответа\n\n"
+            "Нажми /start, чтобы вернутся обратно в меню\n"
+        )
+
+    except Exception as e:
+
+        await message.answer(
+            f"❌ Ошибка:\n{e}"
+        )
+
+    finally:
+
+        await state.clear()
+
+# ==================================================
+# ОТВЕТ УЧАСТНИКА ГРУППЫ
+# ==================================================
+
+@router.message(
+    lambda message:
+    message.chat.id == GROUP_AD
+    and message.reply_to_message is not None
+)
+async def group_reply(message: types.Message, bot: Bot):
+
+    replied_message_id = (
+        message.reply_to_message.message_id
+    )
+
+    # Ищем пользователя, которому принадлежит
+    # сообщение, на которое ответили
+    user_id = group_to_user.get(
+        replied_message_id
+    )
+
+    if not user_id:
+        return
+
+    try:
+
+        # Отправляем ответ пользователю
+        sent = await bot.copy_message(
+            chat_id=user_id,
+            from_chat_id=GROUP_AD,
+            message_id=message.message_id
+        )
+
+        # Теперь сообщение пользователя в личке
+        # связано с сообщением в группе
+        user_to_group[sent.message_id] = (
+            message.message_id
+        )
+
+    except Exception as e:
+
+        print(
+            f"Ошибка отправки ответа: {e}\n\n"
+            "Нажми /start, чтобы вернутся обратно в меню\n"
+        )
+
+# ==================================================
+# ОТВЕТ ПОЛЬЗОВАТЕЛЯ НА СООБЩЕНИЕ ИЗ ГРУППЫ
+# ==================================================
+
+@router.message(
+    lambda message:
+    message.chat.id != GROUP_AD
+    and message.reply_to_message is not None
+)
+async def user_reply(message: types.Message, bot: Bot):
+
+    replied_message_id = (
+        message.reply_to_message.message_id
+    )
+
+    # Находим сообщение в группе,
+    # которому соответствует сообщение в личке
+    group_message_id = user_to_group.get(
+        replied_message_id
+    )
+
+    if not group_message_id:
+        return
+
+    try:
+
+        # Копируем ответ пользователя в группу
+        sent = await bot.copy_message(
+            chat_id=GROUP_AD,
+            from_chat_id=message.chat.id,
+            message_id=message.message_id,
+            reply_parameters=types.ReplyParameters(
+                message_id=group_message_id
+            )
+        )
+
+        # Связываем новое сообщение в группе
+        # с пользователем
+        group_to_user[sent.message_id] = (
+            message.from_user.id
+        )
+
+    except Exception as e:
+
+        print(
+            f"Ошибка отправки ответа: {e}\n\n"
+            "Нажми /start, чтобы вернутся обратно в меню\n"
         )
